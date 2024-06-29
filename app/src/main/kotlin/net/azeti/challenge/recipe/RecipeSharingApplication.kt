5eebed412337ff2138265bcc3264c81182e89743
paulobalmer <paulo@balmersysteme.de>
2024-06-29 22:10:10 +0200

    Initial project structure and creation of database tables using flyway

---
+++ app/src/main/kotlin/net/azeti/challenge/recipe/RecipeSharingApplication.kt
@@ -0,0 +1,11 @@
+package net.azeti.challenge.recipe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecipeSharingApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecipeSharingApplication>(*args)
+}
---
+++ app/src/main/resources/db/migration/V1_1__create_tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE public.users (
+	id uuid NOT NULL,
+	"name" varchar(100) NOT NULL,
+	login varchar(50) NOT NULL,
+	"password" varchar(100) NOT NULL,
+	active bool NOT NULL,
+	created_at timestamp NOT NULL,
+	updated_at timestamp NULL,
+	CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.recipes (
+	id uuid NOT NULL,
+	title varchar(150) NOT NULL,
+	user_id uuid NOT NULL,
+	description text NULL,
+	instructions text NOT NULL,
+	servings text NULL,
+	created_at timestamp NOT NULL,
+	updated_at timestamp NULL,
+	CONSTRAINT recipes_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_recipes_user FOREIGN KEY (user_id) REFERENCES public.users(id)
+);
+
+CREATE TABLE public.ingredients (
+	id uuid NOT NULL,
+	recipe_id uuid NOT NULL,
+	"value" numeric(19, 2) NOT NULL,
+	unit varchar(30) NOT NULL,
+	"type" varchar(120) NOT NULL,
+	created_at timestamp NOT NULL,
+	updated_at timestamp NULL,
+	CONSTRAINT ingredients_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_ingredients_recipe FOREIGN KEY (recipe_id) REFERENCES public.recipes(id)
+);
